3a3b35fa2d1520c188435a6a56dfc3252e8e9efb
Mattrp227 <109999331+Mattrp227@users.noreply.github.com>
2022-09-28 21:49:02 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
+//Generate ID
+const generateID = require('generate-unique-id');
+
+
+//create constructors
+
+const fs = require('fs');
+const express = require("express");
+const notes = require("./db/db.json");
+const app = express();
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+
+
+//create new note
+function newNote(body notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({notes:notesArray}, null, 2));
+        return note;
+
+};
+
 
